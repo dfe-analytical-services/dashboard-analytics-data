8b543de97c4ac9c92917a178e14adc1fa2acf5d3
Rich Bielby <richbielby@e.email>
2026-03-27 14:35:36 +0000

    Cleaning up accounts list code a little

--- R/show_ga_accounts.r
+++ R/show_ga_accounts.r
@@ -1,13 +1,16 @@
 # Databricks notebook source
 # DBTITLE 1,Install and load dependencies
 # COMMAND ----------
-install.packages("pak")
-pak::pak("googleAnalyticsR")
+
+if(system.file(package='as')=="") {install.packages("pak")}
+packages <- c("googleAnalyticsR", "dplyr")
+pak::pak(setdiff(packages, rownames(installed.packages())))
 
 # COMMAND ----------
 
 source("utils.R")
 source("params.R")
-googleAnalyticsR::ga_auth()
-googleAnalyticsR::ga_account_list(type = "ga4")
+googleAnalyticsR::ga_auth(json_file = auth_json_path)
+account_list <- googleAnalyticsR::ga_account_list(type = "ga4")
 
+print(account_list)
